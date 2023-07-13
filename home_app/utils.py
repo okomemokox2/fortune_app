cbd373c7988cafc7fdb4808eb7bc7913022c6ced
okomemokox2 <okomemokox2@icloud.com>
2023-07-13 21:01:41 +0900

    after deploying on heroku

---
+++ home_app/utils.py
@@ -0,0 +1,11 @@
+import requests
+
+def get_moon_image(api_key):
+    url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}&date=today"
+    response = requests.get(url)
+    data = response.json()
+    
+    if response.status_code == 200 and data.get("media_type") == "image":
+        return data.get("url")
+    else:
+        return None
--- home_app/views.py
+++ home_app/views.py
@@ -1,8 +1,33 @@
+from .utils import get_moon_image
 from django.shortcuts import render
-
+from .utils import get_moon_image
+import requests
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
-
 def about(request):
    return render(request, 'about.html') 
+
+def get_moon_image(api_key):
+    url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}&date=today"
+    response = requests.get(url)
+    data = response.json()
+    
+    if response.status_code == 200 and data.get("media_type") == "image":
+        return data.get("url")
+    else:
+        return None
+
+# APIキーを設定
+api_key = "e5iNBJbsZSKCpBcYR0sNo0v8CQu3APKDKtzb7ofS"
+
+# 関数を呼び出して今夜の月の画像URLを取得
+moon_image_url = get_moon_image(api_key)
+
+if moon_image_url:
+    print("今夜の月の画像URL:", moon_image_url)
+else:
+    print("今夜の月の画像を取得できませんでした。")
+
+def home(request):
+    api_key = "e5iNBJbsZSKCpBcYR0sNo0v8CQu3APKDKtzb7ofS"
+    moon_image_url = get_moon_image(api_key)
+    return render(request, 'home.html', {'moon_image_url': moon_image_url})
